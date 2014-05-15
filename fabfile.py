7e5c94db2a3d0877df84ffc1144510fab252d498
Job Ganzevoort <ganzevoort@gw20e.com>
2014-05-15 15:26:42 +0200

    streamline fab commands

--- fabfile.py
+++ fabfile.py
@@ -18,8 +18,10 @@ Usage::
   ./bin/fab <fabric command>:<optional parameter>
 
 """
+
 from fabric.api import env, settings
 from fabric.decorators import task, hosts
+
 
 try:
     from fabric_lib.tasks import (test_connection, pull_modules, restart_instances,
@@ -31,14 +33,15 @@ except ImportError:
     )
     exit(0)
 
+
 ##############
 # Appie config
 ##############
 
-# CHANGE THE FOLLOWING VARIABLE FOR YOUR BUILDOUT / APPIE ENV:
+# CHANGE THE FOLLOWING VARIABLES FOR YOUR BUILDOUT / APPIE ENV:
 
 # Name of the appie environment
-appie_env = 'appie-env'  
+appie_env = 'fabric'
 # Module which can be updated using git pull
 env.modules = ('project.egg', )  
 # Local url to Plone
@@ -46,72 +49,79 @@ env.site_url = 'http://localhost:{0}/plone_id/'
 # Git uri to buildout
 env.buildout_uri = 'git@git.gw20e.com:Project/buildout-name.git'  
 # SSH uri's for acc and prd
-env.acc_host = 'app-{0}-acc@cobain.gw20e.com'.format(appie_env)  
-env.prd_hosts = (
-    'app-{0}-prd@192.168.5.52'.format(appie_env), 
-    'app-{0}-prd@192.168.5.53'.format(appie_env),
-)   
+deploy_info = {
+    'acc': {
+        'hosts': ['app-{0}-acc@cobain.gw20e.com'.format(appie_env)],
+    },
+    'prd': {
+        'hosts': [
+            'app-{0}-prd@192.168.5.52'.format(appie_env), 
+            'app-{0}-prd@192.168.5.53'.format(appie_env),
+        ],
+    },
+}
+
 
 #############
-# Acceptance
+# Env config
+#############
+env.forward_agent = True
+env.always_use_pty = False
+env.linewise = True
+env.shell = '/bin/dash -e -c'
+
+
+#############
+# Tasks
 #############
 @task
-@hosts(env.acc_host)
-def acc_test():
+def check_cluster(layer='acc'):
+    cluster = get_master_slave(deploy_info[layer]['hosts'], quiet=False)
+    print('\n'.join(
+        ['', 'Current cluster info for {0}:'.format(layer)] +
+        ["\t{0} is {1}".format(k,v) for k,v in sorted(cluster.items())] +
+        ['']))
+
+def select_servers(func):
+    def wrapped(layer='acc', server=None, *args, **kwargs):
+        servers = deploy_info[layer]['hosts']
+        if server:
+            matches = [s for s in servers if server in s]
+            if matches:
+                servers = matches
+            else:
+                cluster = get_master_slave(servers)
+                servers = [cluster[server]]
+        for host in servers:
+            print host
+            with settings(host_string=host):
+                func(*args, **kwargs)
+    wrapped.__name__ = func.__name__
+    wrapped.__doc__ = func.__doc__
+    return wrapped
+
+@task
+@select_servers
+def test():
     """ Test connection """
     test_connection()
 
 @task
-@hosts(env.acc_host)
-def acc_update(tag=None):
+@select_servers
+def update(tag=None):
     """ Pull modules in env.modules and restart instances """
     pull_modules(tag=tag)
     restart_instances()
 
 @task
-@hosts(env.acc_host)
-def acc_deploy():
+@select_servers
+def deploy(tag=None):
     """ Create new buildout in release dir """
-    deploy_buildout(tag='master')
+    deploy_buildout(tag=tag)
 
 @task
-@hosts(env.acc_host)
-def acc_switch():
+@select_servers
+def switch(tag=None):
     """ Switch supervisor in current buildout dir to latest buildout """
-    switch_buildout(tag='master')
+    switch_buildout(tag=tag)
 
-#############
-# Production
-#############
-@task
-def prd_test():
-    """ Test connection, returns master/slave hostnames """
-    get_master_slave()
-
-@task
-def prd_deploy(server, tag=None):
-    """ Create new buildout in release dir. 
-    When tag is left empty the current date will be used as tag; ie prd-20140104
-    """
-
-    cluster = get_master_slave()
-
-    with settings(host_string=cluster[server]):
-        deploy_buildout(tag=tag)
-
-@task
-def prd_switch(server, tag=None):
-    """ Switch supervisor in current buildout dir to latest buildout. 
-    When tag is left empty the current date will be used as tag; ie prd-20140104
-    """
-    cluster = get_master_slave()
-
-    with settings(host_string=cluster[server]):
-        switch_buildout(tag=tag)
-
-@task
-@hosts(env.prd_hosts)
-def prd_update(tag=None):
-    """ Pull modules in env.modules and restart instances """
-    pull_modules(tag=tag)
-    restart_instances()
