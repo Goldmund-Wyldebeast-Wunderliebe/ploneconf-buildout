bf687aba18f9f51cba435c3a7c7c1f76474dc617
Kim Chee Leong <leong@gw20e.com>
2014-05-19 15:19:05 +0200

    Moving tasks to fabric_lib

--- fabfile.py
+++ fabfile.py
@@ -19,13 +19,14 @@ Usage::
 
 """
 
-from fabric.api import env, settings
-from fabric.decorators import task, hosts
+from fabric.api import env
+from fabric.decorators import task
+
+from fabric_lib.helpers import select_servers, test_connection
 
 
 try:
-    from fabric_lib.tasks import (test_connection, pull_modules, restart_instances,
-        deploy_buildout, switch_buildout, get_master_slave, prepare_release)
+    from fabric_lib.tasks import (test, update, deploy, switch)
 except ImportError:
     print('To active the fabric_lib submodule run:\n'
           '  git submodule init && git submodule update\n'
@@ -69,59 +70,3 @@ env.forward_agent = True
 env.always_use_pty = False
 env.linewise = True
 env.shell = '/bin/dash -e -c'
-
-
-#############
-# Tasks
-#############
-@task
-def check_cluster(layer='acc'):
-    cluster = get_master_slave(deploy_info[layer]['hosts'], quiet=False)
-    print('\n'.join(
-        ['', 'Current cluster info for {0}:'.format(layer)] +
-        ["\t{0} is {1}".format(k,v) for k,v in sorted(cluster.items())] +
-        ['']))
-
-def select_servers(func):
-    def wrapped(layer='acc', server=None, *args, **kwargs):
-        servers = deploy_info[layer]['hosts']
-        if server:
-            matches = [s for s in servers if server in s]
-            if matches:
-                servers = matches
-            else:
-                cluster = get_master_slave(servers)
-                servers = [cluster[server]]
-        for host in servers:
-            print host
-            with settings(host_string=host):
-                func(*args, **kwargs)
-    wrapped.__name__ = func.__name__
-    wrapped.__doc__ = func.__doc__
-    return wrapped
-
-@task
-@select_servers
-def test():
-    """ Test connection """
-    test_connection()
-
-@task
-@select_servers
-def update(tag=None):
-    """ Pull modules in env.modules and restart instances """
-    pull_modules(tag=tag)
-    restart_instances()
-
-@task
-@select_servers
-def deploy(tag=None):
-    """ Create new buildout in release dir """
-    deploy_buildout(tag=tag)
-
-@task
-@select_servers
-def switch(tag=None):
-    """ Switch supervisor in current buildout dir to latest buildout """
-    switch_buildout(tag=tag)
-
