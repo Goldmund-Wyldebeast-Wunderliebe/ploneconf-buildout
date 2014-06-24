7208d5b5d428335ab63ebe030e8f8715facc394e
Job Ganzevoort <ganzevoort@gw20e.com>
2014-06-25 00:00:49 +0200

    fabric_lib reorg

--- fabfile.py
+++ fabfile.py
@@ -24,16 +24,13 @@ from fabric.api import env
 
 
 try:
-    from fabric_lib.tasks import (
-            check_cluster, test, update, deploy, switch, copy,
-            prepare_release,
-            )
+    from fabric_lib import *
 except ImportError:
     print('To activate the fabric_lib submodule run:\n'
           '  git submodule init && git submodule update\n'
           'For more info read https://intranet.gw20e.com/development/plone-projecten/fabric-deployment'
     )
-    import fabric_lib.tasks  # Just to show the real error message
+    import fabric_lib  # Just to show the real error message
     exit(0)
 
 
