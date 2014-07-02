30ad3c8e980b59a64c474e4f2087e165153b4484
Job Ganzevoort <ganzevoort@gw20e.com>
2014-07-02 23:47:04 +0200

    buildout-hhd on dhansak/patia

--- deployment.py
+++ deployment.py
@@ -1,30 +1,47 @@
+_env_name = 'hhd'
+_flying_ip = '91.194.224.146'
+_servers = {'dhansak': '192.168.3.44', 'patia': '192.168.3.45'}
+
+_base_port = 8210
+# tst instances use _base_port +1
+# acc instances use _base_port +2, +3 (instances)
+#     zeo uses                 +10002
+#     haproxy uses             +20002
+# prd instances use _base_port +4 ... +7
+#     zeo uses                 +10004
+#     haproxy uses             +20004
+#     varnish uses             +40004
+
 
 tst = dict(
-    hosts= ['app-fabric-acc@cobain.gw20e.com'],
+    hosts= ['app-%s-tst@%s' % (_env_name, s) for s in _servers.values()],
     buildout= 'buildout-tst',
     modules = {
         'gww.imprint': 'git git@git.gw20e.com:gww/gww-imprint.git',
     },
-    instances= { 'ports': { 'instance0': 8194, }, },
-    credentials= { 'username': 'app-fabric-tst', 'password': 'keuteltje14', },
+    instances= { 'ports': { 'instance0': _base_port+1, }, },
+    credentials= { 'username': 'admin', 'password': 'secret', },
     site_id = 'mysite',
     webserver= 'apache2',
-    sitename= 'fabric-tst.gw20e.com',
+    sitename= '%s-tst.gw20e.com' % _env_name,
 )
 
 acc = dict(
-    hosts= ['app-fabric-acc@cobain.gw20e.com'],
+    hosts= ['app-%s-acc@%s' % (_env_name, s) for s in _servers.values()],
     buildout= 'buildout-acc',
-    haproxy= { 'port': 28195 },
-    zeo= { 'port': 18195, 'base': '/opt/APPS/fabric/acc/db', },
+    haproxy= { 'port': _base_port + 20002 },
+    zeo= {
+        'port': _base_port + 10002,
+        'base': '/opt/APPS/%s/acc/db' % _env_name,
+    },
     instances= {
-        'ports': { 'instance0': 8195, 'instance1': 8196, },
+        'ports': { 'instance0': _base_port + 2, 'instance1': _base_port + 3, },
         'ipaddresses': { 'localhost': '127.0.0.1', },
     },
-    credentials= { 'username': 'app-fabric-acc', 'password': 'keuteltje14', },
+    credentials= { 'username': 'admin', 'password': 'secret', },
     site_id = 'mysite',
     webserver= 'apache2',
-    sitename= 'fabric-acc.gw20e.com',
+    sitename= '%s-acc.gw20e.com' % _env_name,
 )
 
 
@@ -34,23 +51,20 @@ def _datestamped(fmt):
     return now.strftime(fmt)
 
 prd = dict(
-    hosts= [
-        'app-fabric-prd@192.168.5.52',
-        'app-fabric-prd@192.168.5.53',
-    ],
+    hosts= ['app-%s-prd@%s' % (_env_name, s) for s in _servers.values()],
     buildout= _datestamped('releases/%Y-%m-%d'),
     current_link = 'current',
     auto_switch = False,
     zeo= {
-        'ip': '91.194.224.154',
-        'port': 18450,
+        'ip': _flying_ip,
+        'port': _base_port + 10004,
     },
     instances= {
-        'ports': {'instance{}'.format(i): 8450+i for i in range(4)},
-        'ipaddresses': { 'madras': '192.168.5.52', 'saag': '192.168.5.53', },
+        'ports': {'instance{}'.format(i): _base_port + 4 + i for i in range(4)},
+        'ipaddresses': _servers,
     },
     site_id = 'mysite',
-    credentials= { 'username': 'app-fabric-prd', 'password': 'keuteltje14', },
+    credentials= { 'username': 'admin', 'password': 'secret', },
     sentry= {
         'dsn': 'https://sentry_api_key:example@sentry.gw20e.com/xx',
         'level': 'ERROR',
@@ -60,17 +74,17 @@ prd = dict(
 prdfe = dict(
     hosts = prd['hosts'],
     buildout= 'releases/frontend',
-    varnish= { 'port': 48450 },
-    haproxy= { 'port': 28450, 'instances': prd['instances'], },
+    varnish= { 'port': _base_port + 40004 },
+    haproxy= { 'port': _base_port + 20004, 'instances': prd['instances'], },
     webserver= 'apache2',
-    sitename= 'fabric-www.gw20e.com',
+    sitename= '%s-prd.gw20e.com' % _env_name,
     site_id = 'mysite',
 )
 
 prdbe = dict(
     hosts = prd['hosts'],
     buildout= 'releases/backend',
-    zeo = dict(base= '/data1/APPS/fabric/prd', **prd['zeo']),
+    zeo = dict(base= '/data1/APPS/%s/prd' % _env_name, **prd['zeo']),
 )
 
 
