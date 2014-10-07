fd225af543536b6f05ee3e0804145f8501bd40d4
Kim Chee Leong <leong@gw20e.com>
2014-10-07 10:24:24 +0200

    Moving back to a buildout without a shared base

--- deployment.py
+++ deployment.py
@@ -1,21 +1,105 @@
-from base.deployment import (_modules, get_dvl, get_tst, get_acc,
-    get_prd, get_prdbe, get_prdfe, templates)
+_base_port = 8080
+_env_name = 'fabric'
+_site_id = 'Plone'
 
-_base_port = 8210
-_env_name = 'hhd'
-_site_id = 'hhd_intranet'
+_flying_ip = '91.194.224.146'
+_servers = {'dhansak': '192.168.3.44', 'patia': '192.168.3.45'}
 
-_modules.update({
-    'hhd.theme.intranet': 'git git@git.gw20e.com:waterschappen/hhd-theme-intranet.git',
-    'hhd.theme.internet': 'git git@git.gw20e.com:waterschappen/hhd-theme-internet.git',
-})
+_modules = {
+    'gww.imprint': 'git git@git.gw20e.com:gww/gww-imprint.git',
+}
 
-dvl = get_dvl(_base_port, _env_name, _site_id)
-tst = get_tst(_base_port, _env_name, _site_id)
-acc = get_acc(_base_port, _env_name, _site_id)
-prd = get_prd(_base_port, _env_name, _site_id)
-prdbe = get_prdbe(_base_port, _env_name, prd)
-prdfe = get_prdfe(_base_port, _env_name, prd)
+_third_party_modules = {
+    'collective.autopublishing': 'git https://github.com/collective/collective.autopublishing.git',
+}
 
+def _datestamped(fmt):
+    from datetime import datetime
+    now = datetime.now()
+    return now.strftime(fmt)
+
+tst = dict(
+    hosts=['app-%s-tst@%s' % (_env_name, s) for s in _servers.values()],
+    buildout='buildout-tst',
+    haproxy={'port': _base_port + 20002},
+    modules=_modules,
+    third_party_modules=_third_party_modules,
+    zeo={
+        'ip': _flying_ip,
+        'port': _base_port + 20001,
+        'base': '/opt/APPS/%s/tst/db' % _env_name,
+    },
+    instances={
+        'ports': {'instance0': _base_port + 2, },
+        'ipaddresses': _servers,
+    },
+    credentials={'username': 'admin', 'password': 'secret', },
+    __site_id=_site_id,
+    webserver='apache2',
+    sitename='%s-tst.gw20e.com' % _env_name,
+    remote_configs={'clockusers': 'clockuser.cfg'},
+)
+
+acc = dict(
+    hosts=['app-%s-acc@%s' % (_env_name, s) for s in _servers.values()],
+    buildout='buildout-acc',
+    haproxy={'port': _base_port + 30002},
+    modules=_modules,
+    third_party_modules=_third_party_modules,
+    zeo={
+        'ip': _flying_ip,
+        'port': _base_port + 30001,
+        'base': '/opt/APPS/%s/acc/db' % _env_name,
+    },
+    instances={
+        'ports': {'instance0': _base_port + 3, 'instance1': _base_port + 4, },
+        'ipaddresses': _servers,
+    },
+    credentials={'username': 'admin', 'password': 'secret', },
+    _site_id=_site_id,
+    webserver='apache2',
+    sitename='%s-acc.gw20e.com' % _env_name,
+    remote_configs={'clockusers': 'clockuser.cfg'},
+)
+
+prd = dict(
+    hosts=['app-%s-prd@%s' % (_env_name, s) for s in _servers.values()],
+    buildout=_datestamped('releases/%Y-%m-%d'),
+    current_link='current',
+    auto_switch=False,
+    modules=_modules,
+    third_party_modules=_third_party_modules,
+    zeo={
+        'ip': _flying_ip,
+        'port': _base_port + 40001,
+    },
+    instances={
+        'ports': {'instance{}'.format(i): _base_port + 5 + i for i in range(4)},
+        'ipaddresses': _servers,
+    },
+    _site_id=_site_id,
+    credentials={'username': 'admin', 'password': 'secret', },
+    remote_configs={'clockusers': 'clockuser.cfg'},
+    sentry={
+        'dsn': 'https://406ca4faab4b4dfd9c40f5ff790d5294:012c8b89ccb4420582d38d474c26cd11@sentry.gw20e.com/29',
+        'level': 'ERROR',
+    },
+)
+
+prdfe = dict(
+    hosts=prd['hosts'],
+    buildout='releases/frontend',
+    varnish={'port': _base_port + 40003},
+    haproxy={'port': _base_port + 40002, 'instances': prd['instances'], },
+    webserver='apache2',
+    sitename='%s-prd.gw20e.com' % _env_name,
+    _site_id=prd['_site_id'],
+)
+
+prdbe = dict(
+    hosts=prd['hosts'],
+    buildout='releases/backend',
+    zeo=dict(base='/data1/APPS/%s/prd' % _env_name, **prd['zeo']),
+)
 
 default = 'tst'
--- doc/README.txt
+++
@@ -1,4 +0,0 @@
-These buildout files were copied from the GW20e buildout-template
-(https://svn.gw20e.com/gw20e-plone/buildout-template).
-To see the rationale behind this product and how to use it, see
-https://intranet.gw20e.com/beheer/documentatie/uniforme-buildout-naamgevingen
